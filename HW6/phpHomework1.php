a4819da8356927db0e45c7b57092eda8482684ce
Scarbrain <Scarbrain@abv.bg>
2016-11-09 14:57:18 +0200

    09/11

--- HW6/phpHomework1.php
+++ HW6/phpHomework1.php
@@ -4,7 +4,7 @@ $name = 'Johnny';
 $address = 'Knoxville';
 $phone_number = 1234567890;
 
-echo $name; 
+echo $name;
 echo '<br/>';
 echo $address;
 echo '<br/>';
@@ -13,4 +13,12 @@ echo '<br/>';
 
 
 echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+echo '<br/>';
+echo 'Current PHP version: ' . phpversion();
+echo '<br/>';
+
+$file = 'phpHomework1.php';
+if (file_exists($file)) {
+    echo "$file was last modified: " . date ("F d Y H:i:s.", filemtime($file));
+}
 
