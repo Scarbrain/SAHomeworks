f7e4d3855ace4e86def655bb5b88c3eed11c7e7d
Scarbrain <scarbrain@abv.bg>
2017-11-01 15:56:24 +0200

    Add files via upload

---
+++ c++/regularEx/cmake-build-debug/CMakeFiles/regularEx.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/regularEx.dir/main.cpp.obj"
+  "regularEx.pdb"
+  "regularEx.exe"
+  "regularEx.exe.manifest"
+  "libregularEx.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/regularEx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
