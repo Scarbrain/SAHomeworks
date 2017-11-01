8b54c0aea6ddfd29268a6e7613f4359f294b1d2d
Scarbrain <scarbrain@abv.bg>
2017-11-01 15:51:51 +0200

    Add files via upload

---
+++ c++/AckermannFunction/cmake-build-debug/CMakeFiles/AckermannFunction.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AckermannFunction.dir/main.cpp.obj"
+  "AckermannFunction.pdb"
+  "AckermannFunction.exe"
+  "AckermannFunction.exe.manifest"
+  "libAckermannFunction.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AckermannFunction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
